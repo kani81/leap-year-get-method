5ccd3f5ff3d731c3b5a92b16841b1ae0e5ae403b
kani81 <kaniarasiduraisamy.com>
2023-08-02 11:47:40 +0530

    addnew

---
+++ main.js
@@ -0,0 +1,10 @@
+var date=new Date();
+var year=date.getFullYear();
+document.write("Given year="+year+"<br><br>")
+if(year%4==0){
+	document.write("this year is leap year"+"<br>")
+}
+else{
+	document.write("this year is not leap year"+"<br>")
+}
+	
